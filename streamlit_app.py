372286b0c509866110ee24d3f5d9bf19f59e6f09
jorgejuan007 <jorgejuan007@gmail.com>
2022-11-14 18:47:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,10 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Bienvenido a Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+Ejemplo de Software Experimental de Legal Intermedia
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 """
 
 
